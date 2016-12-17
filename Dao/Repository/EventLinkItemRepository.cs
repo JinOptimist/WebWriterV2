ad20920e1a35c9853cd185073df7f7ea0eacdcb5
88jin@mail.ru <88jin@mail.ru>
2016-12-17 11:02:02 +0300

    repair reinit with EventLinkItem

--- Dao/Repository/EventLinkItemRepository.cs
+++ Dao/Repository/EventLinkItemRepository.cs
@@ -11,8 +11,8 @@ namespace Dao.Repository
 
         public override EventLinkItem Save(EventLinkItem model)
         {
-            model.From = new Event {Id = model.From.Id};
-            model.To = new Event {Id = model.To.Id};
+            //model.From = new Event {Id = model.From.Id};
+            //model.To = new Event {Id = model.To.Id};
             return base.Save(model);
         }
     }
