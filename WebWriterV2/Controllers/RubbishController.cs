11ff4727ef988c58612dc9432877f463011df985
Pavel_Lvou <Pavel_Lvou@epam.com>
2016-12-24 13:21:37 +0300

    Add Rubbish controller for work

---
+++ WebWriterV2/Controllers/RubbishController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebWriterV2.Controllers
+{
+    public class RubbishController : Controller
+    {
+        //
+        // GET: /Rubbish/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
