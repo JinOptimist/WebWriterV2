fe83961b127acfb1dc7e7a3864be4c6e67483cda
Pavel_Lvou <Pavel_Lvou@epam.com>
2017-02-06 16:38:07 +0300

    Add one more test

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,39 +6,51 @@ using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        static string rpgBaseUrl = "http://martin-s.by/AngularRoute/listQuest";
+
+        static string secondBefor = "stateType-60072-Value";
         static void Main(string[] args)
         {
-            var rpgBaseUrl = "http://martin-s.by/AngularRoute/listQuest";
+            var towerId = "quest-32";
             var sevenMinutesId = "quest-33";
-            var secondBefor = "stateType-60072-Value";
-            IWebDriver driver = new ChromeDriver();
+            var kgbId = "quest-34";
+            var driver = new ChromeDriver();
 
-            try
+            MyTest(driver, towerId);
+            driver.Manage().Window.Maximize();
+            MyTest(driver, sevenMinutesId);
+
+            //MyTest(driver, kgbId);
+
+            Thread.Sleep(1 * 1000);
+            driver.Quit();
+        }
+
+        static void MyTest(IWebDriver driver, string questName)
+        {
+            driver.Navigate().GoToUrl(rpgBaseUrl);
+
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
+
+            //driver.FindElements(By.CssSelector("." + questName + " div"))[0].Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id(questName)).Click();
+            //var sec = driver.FindElement(By.Id(secondBefor)).Text;
+
+            for (int i = 0; i < 5; i++)
             {
-                driver.Navigate().GoToUrl(rpgBaseUrl);
-                driver.Manage().Window.Maximize();
-
-                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-                driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
-
-                driver.FindElement(By.Id(sevenMinutesId)).Click();
-                //var sec = driver.FindElement(By.Id(secondBefor)).Text;
-
-                for (int i = 0; i < 11; i++)
-                {
-                    Thread.Sleep(200);
-                    driver.FindElements(By.TagName("li"))[0].Click();
-                }
-            }
-            finally
-            {
-                Thread.Sleep(1 * 1000);
-                driver.Quit();
+                Thread.Sleep(200);
+                driver.FindElements(By.TagName("li"))[0].Click();
             }
         }
     }
