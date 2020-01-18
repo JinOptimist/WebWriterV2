78a4ba9da543d701c9841c4a846f9260ada33140
88jin@mail.ru <88jin@mail.ru>
2020-01-18 14:06:27 +0300

    Return functionality Publish

--- WebWriterV2/Scripts/rpg/writer/books-controller.js
+++ WebWriterV2/Scripts/rpg/writer/books-controller.js
@@ -19,17 +19,17 @@ angular.module('rpg')
 
             $scope.showCreateBook = function () {
                 $scope.newBook = {};
-            }
+            };
 
             $scope.switchToEditModeBookForNameOrDesc = function (book) {
                 book.actionsActive = false;
                 book.isEdit = true;
-            }
+            };
 
             $scope.toggleActionsBlock = function ($event, book) {
                 book.actionsActive = !book.actionsActive;
                 $event.stopPropagation();
-            }
+            };
 
             $scope.bookNameTyped = function ($event) {
                 // 'enter'.keyEvent === 13
@@ -38,7 +38,7 @@ angular.module('rpg')
                 }
 
                 $scope.createBook();
-            }
+            };
 
             $scope.remove = function (book, index) {
                 if (!confirm(resources.Writer_ConfirmRemovingBook.format(book.Name))) {
@@ -48,36 +48,36 @@ angular.module('rpg')
                     .then(function () {
                         $scope.books.splice(index, 1);
                     });
-            }
+            };
 
             $scope.filterBooks = function (filter) {
                 $scope.filter = filter.restriction;
                 $scope.listOfFilters.forEach(filter => filter.active = false);
                 filter.active = true;
-            }
+            };
 
             $scope.togglePublish = function (book) {
                 bookService.publishBook(book.Id, !book.IsPublished).then(function () {
                     book.IsPublished = !book.IsPublished;
                 });
-            }
+            };
 
             $scope.updateBookNameAndDesc = function (book) {
                 bookService.saveBook(book).then(function (newBook) {
                     book.isEdit = false;
                 });
-            }
+            };
 
             $scope.documentKeyPressed = function (e) {
                 // 'esc'.which == 27
                 if (e.which === 27) {
                     $scope.cancelCreationNewBook();
                 }
-            }
+            };
 
             $scope.cancelCreationNewBook = function () {
                 $scope.newBook = null;
-            }
+            };
 
             $scope.tagAdd = function (e, book) {
                 // 'enter'.which == 13
@@ -94,13 +94,13 @@ angular.module('rpg')
                         book.newTag = {};
                     });
                 }
-            }
+            };
 
             $scope.removeTag = function (book, tag, tagIndex) {
                 tagService.removeTagFromBook(book.Id, tag.Id).then(function () {
                     book.Tags.splice(tagIndex, 1);
                 });
-            }
+            };
 
             $scope.createBook = function() {
                 if (!$scope.newBook.name) {
@@ -117,7 +117,7 @@ angular.module('rpg')
                 });
 
                 $scope.newBook = null;
-            }
+            };
 
             function loadBooks() {
                 bookService.getAllForWriter().then(function (books) {
